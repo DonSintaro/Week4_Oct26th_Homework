61690c93def951d319f6bf1c36c513410195b814
DonSintaro <jarriaga.important@gmail.com>
2019-10-29 18:08:18 -0500

    Javascript Objects progress

---
+++ JavascriptQuest.js
@@ -0,0 +1,59 @@
+// class Question {
+//     constructor( question, arrayC , answer){
+
+//         this.question = question;
+//         this.arrayC = arrayC;
+//         this.answer = answer;
+//     }
+
+//     get checkUserChoice (){
+//         if (userChoice == answer){
+//             return true;
+//         }
+//         else {
+//             return false;
+//         }
+//     }
+
+// }
+
+var userChoice;
+
+var Questions = [
+    {
+        question: "Which object allows you to access functions to calculate mathematical task on numbers?",
+        choices: ["Function.", "Object.","Math.","Calc."],
+        answer: "Math."
+    },/////
+
+    {
+        question: "What are the three building blocks of web development?",
+        choices: ["Google, Facebook, Shrome", "Math, Language, Internet Speed", "C++, Python, Heroku", "HTML, CSS, Javascript"],
+        answer: "HTML, CSS, Javascript"
+    },/////
+
+    {
+        question: "The Skeleton of a Website is?",
+        choices: ["HTML","CSS","Javascript","Bootstrap"],
+        answer: "HTML"
+    },/////
+
+    {
+        question: "",
+        choices: [],
+        answer: ""
+    },/////
+
+    {
+        question: "",
+        choices: [],
+        answer: ""
+    },/////
+
+]
+
+
+
+
+
+var questionsA = [one,]
