b5185d73fc315864699ac1d3f23dd266fac88516
DonSintaro <jarriaga.important@gmail.com>
2019-10-31 18:14:17 -0500

    refined HTML/CSS to have targets for JS

--- JavascriptQuest.js
+++ JavascriptQuest.js
@@ -17,7 +17,78 @@
 
 // }
 
+var innerC = document.querySelector("#innerC");
 var userChoice;
+var score;
+
+
+
+var intro = '<div class="col-md-7 col-12"><p>Welcome to a javascript timed quiz.</p><p>Complete the quiz as fast as you can!</p><p class="noMarPad">The scores will be based accuracy of results and the speed of resulting.</p></div><div class="col-md-5 col-12" ><button id="startB" type="button" class="btn btn-dark"><h3>Start Quiz</h3></button></div>';
+var quiz = '<div class="questionTitle"></div><form><input id="choiceA" type="radio" name="answer" value=""> Something<br><input id="choiceB" type="radio" name="answer" value=""> Something<br><input id="choiceC" type="radio" name="answer" value=""> Something<br><input id="choiceD" type="radio" name="answer" value=""> Something<br></form>';
+
+
+
+
+
+
+innerC.innerHTML = intro;
+var startBtn = document.querySelector("#startB");
+
+
+startBtn.addEventListener("click",function(){
+    innerC.innerHTML = quiz;   
+    var questionT = document.querySelector("#questionTitle");
+    var choiceA = document.querySelector("#choiceA");
+    var choiceB = document.querySelector("#choiceB");
+    var choiceC = document.querySelector("#choiceC");
+    var choiceD = document.querySelector("#choiceD"); 
+
+
+
+    
+});
+
+
+
+
+
+
+// choiceA.addEventListener("click",function(){
+    
+// });
+// choiceB.addEventListener("click",function(){
+    
+// });
+// choiceC.addEventListener("click",function(){
+    
+// });
+// choiceD.addEventListener("click",function(){
+    
+// });
+
+function sendQuestion(x,i){
+    questionT.innerHTML = x[i].question;
+
+}
+
+
+function reorder(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    while (0 !== currentIndex) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+
 
 var Questions = [
     {
@@ -33,27 +104,26 @@ var Questions = [
     },/////
 
     {
-        question: "The Skeleton of a Website is?",
+        question: "The skeleton of a Website is built with?",
         choices: ["HTML","CSS","Javascript","Bootstrap"],
         answer: "HTML"
     },/////
 
     {
-        question: "",
-        choices: [],
-        answer: ""
+        question: "The styling of a Website is built with?",
+        choices: ["HTML","CSS","Javascript","Bootstrap"],
+        answer: "CSS"
     },/////
 
     {
-        question: "",
-        choices: [],
-        answer: ""
+        question: "The behavior of a Website is built with?",
+        choices: ["HTML","CSS","Javascript","Bootstrap"],
+        answer: "Javascript"
     },/////
-
-]
-
+];
 
 
 
 
-var questionsA = [one,]
+
+
