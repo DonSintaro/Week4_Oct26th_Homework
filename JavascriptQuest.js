5a3eb99e0c4da4bd939db05a6b3b63399e064114
DonSintaro <jarriaga.important@gmail.com>
2019-11-02 02:02:05 -0500

    more progress on html,javascript

--- JavascriptQuest.js
+++ JavascriptQuest.js
@@ -1,32 +1,18 @@
-// class Question {
-//     constructor( question, arrayC , answer){
-
-//         this.question = question;
-//         this.arrayC = arrayC;
-//         this.answer = answer;
-//     }
-
-//     get checkUserChoice (){
-//         if (userChoice == answer){
-//             return true;
-//         }
-//         else {
-//             return false;
-//         }
-//     }
-
-// }
 
 var innerC = document.querySelector("#innerC");
 var userChoice;
-var score;
+var score = 100;
 
+var highestScores = []; // Use some function that grabs the first 5 in sorted array of scores
+var arrayScores = []; // All of scores obtained through get on storage
 
-
+var questionT;
+var multiChoices = [];
+var counter = 0;
+var Btn = [];
 var intro = '<div class="col-md-7 col-12"><p>Welcome to a javascript timed quiz.</p><p>Complete the quiz as fast as you can!</p><p class="noMarPad">The scores will be based accuracy of results and the speed of resulting.</p></div><div class="col-md-5 col-12" ><button id="startB" type="button" class="btn btn-dark"><h3>Start Quiz</h3></button></div>';
-var quiz = '<div class="questionTitle"></div><form><input id="choiceA" type="radio" name="answer" value=""> Something<br><input id="choiceB" type="radio" name="answer" value=""> Something<br><input id="choiceC" type="radio" name="answer" value=""> Something<br><input id="choiceD" type="radio" name="answer" value=""> Something<br></form>';
-
-
+var quiz = '<div><div id="questionTitle"></div><form><input class="choiceA choice" type="radio" name="answer" value=""><span class="choiceA"> Something</span><br><input class="choiceB choice" type="radio" name="answer" value=""><span class="choiceB"> Something</span><br><input class="choiceC choice" type="radio" name="answer" value=""><span class="choiceC"> Something</span><br><input class="choiceD choice" type="radio" name="answer" value=""><span class="choiceD"> Something</span><br></form></div><div id="result"></div>';
+var credits ='';
 
 
 
@@ -37,37 +23,58 @@ var startBtn = document.querySelector("#startB");
 
 startBtn.addEventListener("click",function(){
     innerC.innerHTML = quiz;   
-    var questionT = document.querySelector("#questionTitle");
-    var choiceA = document.querySelector("#choiceA");
-    var choiceB = document.querySelector("#choiceB");
-    var choiceC = document.querySelector("#choiceC");
-    var choiceD = document.querySelector("#choiceD"); 
+    document.querySelector("#pageHeading").innerHTML = "Quiz Time!";
+    questionT = document.querySelector("#questionTitle");
+    
+
+    document.querySelectorAll(".choice").forEach(function(iter){
+        iter.addEventListener("click",function(){
+            userChoice = document.querySelector('.choice:checked').innerHTML;
+            if (userChoice === Questions[counter-1]){
+                score = score + 5;
+                //prompt correct #result            --------------
+            }
+            else{
+                //prompt wrong #result           -----------
+            }
+            iter.checked = false;
+            if(counter < 5){
+            promptQuestion(counter);
+            }
+            else{
+                // Go to score page
+                // score pages goes to high score page and local storage of scores in order of highest and retry?
+                // retry eventlistener that clears page and goes to intro 
+            }
+        });
+    });
 
 
 
+    multiChoices.push(".choiceA");
+    multiChoices.push(".choiceB");
+    multiChoices.push(".choiceC");
+    multiChoices.push(".choiceD"); 
+    Questions = reorder(Questions);
+    promptQuestion(counter);
     
 });
 
 
 
+function promptQuestion(iNum){
+    questionT.innerHTML = Questions[iNum].question;
+    Questions[iNum].choices = reorder(Questions[iNum].choices);
+    promptChoices(iNum);
+    counter++;
 
+};
 
-
-// choiceA.addEventListener("click",function(){
-    
-// });
-// choiceB.addEventListener("click",function(){
-    
-// });
-// choiceC.addEventListener("click",function(){
-    
-// });
-// choiceD.addEventListener("click",function(){
-    
-// });
-
-function sendQuestion(x,i){
-    questionT.innerHTML = x[i].question;
+function promptChoices(x){
+    for (var i = 0;i < Questions[x].choices.length;i++){
+        document.querySelectorAll(multiChoices[i])[0].innerHTML = Questions[x].choices[i];
+        document.querySelectorAll(multiChoices[i])[1].innerHTML = Questions[x].choices[i];
+    }       
 
 }
 
@@ -92,33 +99,33 @@ function reorder(array) {
 
 var Questions = [
     {
-        question: "Which object allows you to access functions to calculate mathematical task on numbers?",
-        choices: ["Function.", "Object.","Math.","Calc."],
-        answer: "Math."
+        question: "<h4>Which object allows you to access functions to calculate mathematical task?</h4>",
+        choices: [" Function.", " Object."," Math."," Calc."],
+        answer: " Math."
     },/////
 
     {
-        question: "What are the three building blocks of web development?",
-        choices: ["Google, Facebook, Shrome", "Math, Language, Internet Speed", "C++, Python, Heroku", "HTML, CSS, Javascript"],
-        answer: "HTML, CSS, Javascript"
+        question: "<h4>What are the three building blocks of web development?</h4>",
+        choices: [" Google, Facebook, Shrome", " Math, Language, Internet Speed", " C++, Python, Heroku", " HTML, CSS, Javascript"],
+        answer: " HTML, CSS, Javascript"
     },/////
 
     {
-        question: "The skeleton of a Website is built with?",
-        choices: ["HTML","CSS","Javascript","Bootstrap"],
-        answer: "HTML"
+        question: "<h4>The skeleton of a Website is built with?</h4>",
+        choices: [" HTML"," CSS"," Javascript"," Bootstrap"],
+        answer: " HTML"
     },/////
 
     {
-        question: "The styling of a Website is built with?",
-        choices: ["HTML","CSS","Javascript","Bootstrap"],
-        answer: "CSS"
+        question: "<h4>The styling of a Website is built with?</h4>",
+        choices: [" HTML"," CSS"," Javascript"," Bootstrap"],
+        answer: " CSS"
     },/////
 
     {
-        question: "The behavior of a Website is built with?",
-        choices: ["HTML","CSS","Javascript","Bootstrap"],
-        answer: "Javascript"
+        question: "<h4>The behavior of a Website is built with?</h4>",
+        choices: [" HTML"," CSS"," Javascript"," Bootstrap"],
+        answer: " Javascript"
     },/////
 ];
 
